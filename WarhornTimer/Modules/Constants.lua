b5ee16765d45b86eca9a2085da67006a227599e3
uta <uta.karas@gmail.com>
2018-05-21 18:58:37 +0900

    v1.18.2

--- WarhornTimer/Modules/Constants.lua
+++ WarhornTimer/Modules/Constants.lua
@@ -1,20 +1,11 @@
 WarhornTimer = {
   name    = 'WarhornTimer',
-  version = '1.18.1',
+  version = '1.18.2',
   author  = 'Uta',
 }
 
 WarhornTimer.abilityIdList = {
-  [38563] = true, -- War Horn I           #TODO: confirm AbilityId
-  [46526] = true, -- War Horn II          #TODO: confirm AbilityId
-  [46528] = true, -- War Horn III         #TODO: confirm AbilityId
-  [46530] = true, -- War Horn IV
-  [40224] = true, -- Aggressive Horn I
-  [46532] = true, -- Aggressive Horn II
-  [46535] = true, -- Aggressive Horn III
-  [46538] = true, -- Aggressive Horn IV
-  [40221] = true, -- Sturdy Horn I
-  [46541] = true, -- Sturdy Horn II
-  [46544] = true, -- Sturdy Horn III
-  [46547] = true, -- Sturdy Horn IV
+  [38564] = true, -- War Horn
+  [40224] = true, -- Aggressive Horn
+  [40221] = true, -- Sturdy Horn
 }
